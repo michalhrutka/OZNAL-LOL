4011d2d9caea360690f3e24171b195111b589ea6
michal.hrutka <miso.hrutka@gmail.com>
2018-03-23 08:41:12 +0100

    add data_merging.py, update changelog

---
+++ Data preprocessing/data_merging.py
@@ -0,0 +1,50 @@
+import pandas as pd
+
+# read data
+participants_data = pd.read_csv('./../data/participants.csv')
+stats_data = pd.read_csv('./../data/stats_merged.csv')
+champions_data = pd.read_csv('./../data/champs.csv')
+matches_data = pd.read_csv('./../data/matches.csv')
+
+# Drop columns (Probably useless for now)
+# stats_data.drop(['trinket', 'killingsprees', 'largestcrit', 'totheal', 'totunitshealed', 'dmgselfmit', 'dmgtoobj',
+#                  'dmgtoturrets', 'visionscore', 'goldspent', 'pinksbought', 'wardsbought', 'wardsplaced',
+#                  'wardskilled'], axis=1, inplace=True)
+
+# Print column names
+print(participants_data.columns)
+print(champions_data.columns)
+
+# Print first 5 data samples
+print(participants_data.head(5).to_string())
+
+# Add champion names into participants data
+participants_data = pd.merge(left=participants_data, right=champions_data, how='left', left_on='championid',
+                             right_on='id',  suffixes=('', '_y'))
+
+# Drop redundant column created by merge
+participants_data.drop(['id_y'], axis=1, inplace=True)
+print(participants_data.head().to_string())
+
+# Select desired columns from the stats dataset
+stats_data = stats_data[['id', 'win', 'champlvl', 'item1', 'item2', 'item3', 'item4', 'item5', 'item6']]
+
+# Add stats data to participants
+dataset = pd.merge(participants_data, stats_data, how='left', left_on='id', right_on='id',  suffixes=('', '_y'))
+
+# Add matches data to dataset
+dataset = pd.merge(dataset, matches_data, how='left', left_on='matchid', right_on='id',  suffixes=('', '_y'))
+
+# Print statistics and first 5 rows
+print(dataset.describe().to_string())
+print(dataset.head().to_string())
+
+# Print unique values in each column
+for col in dataset.columns:
+    print(col, ':', dataset[col].unique(), '\n')
+
+# One hot encoding of several columns
+one_hot_encoded_champs = pd.get_dummies(dataset['name'], prefix='', prefix_sep='')
+one_hot_encoded_roles = pd.get_dummies(dataset['role'], prefix='', prefix_sep='')
+one_hot_encoded_positions = pd.get_dummies(dataset['position'], prefix='', prefix_sep='')
+one_hot_encoded_items = pd.get_dummies(dataset['item1'], prefix='', prefix_sep='')
--- README.md
+++ README.md
@@ -1,8 +1,12 @@
 # OZNAL-LOL Project
 
-## Step 1
+### Step 1
 * Download data from https://www.kaggle.com/paololol/league-of-legends-ranked-matches/data
 * Extract into OZNAL-LOL/data
 
-## Step 2
-* run `file_merging.py` script
+### Step 2
+* Run `file_merging.py` script
+* Make sure `data/stats_merged.csv` was created
+
+### Step 3
+* Now you are all set to create new scripts or continue working on `data_merging.py`
